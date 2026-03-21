6dfed378469d0aadcbb776e3ead533c4610ed7a6
saachishetty28-art <saachishetty28@gmail.com>
2026-03-21 10:50:33 +0530

    boolean constant example in C

---
+++ module 1/boolean constant.c
@@ -0,0 +1,10 @@
+// boolean constant
+#include<stdio.h>
+#include<stdbool.h>
+void main()
+{
+    bool a=true;
+    bool b=false;
+    printf("the value of a is %d\n",a);
+    printf("the value of b is %d",b);
+}
